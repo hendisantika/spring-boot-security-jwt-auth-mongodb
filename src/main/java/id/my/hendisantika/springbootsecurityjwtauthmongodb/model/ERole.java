679e9c1ba0e0f3d9772987a204cabc0e23766e31
hendisantika <hendisantika@yahoo.co.id>
2024-04-02 06:54:19 +0700

    Add ERole Model 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootsecurityjwtauthmongodb/model/ERole.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.springbootsecurityjwtauthmongodb.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-auth-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/2/24
+ * Time: 06:54
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
