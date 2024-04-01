b4ec6497947a3140a2dc184d10fbaef8f7123284
hendisantika <hendisantika@yahoo.co.id>
2024-04-02 06:55:38 +0700

    Add User Model 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootsecurityjwtauthmongodb/model/User.java
@@ -0,0 +1,55 @@
+package id.my.hendisantika.springbootsecurityjwtauthmongodb.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-auth-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/2/24
+ * Time: 06:55
+ * To change this template use File | Settings | File Templates.
+ */
+@Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    private String password;
+
+    @DBRef
+    private Set<Role> roles = new HashSet<>();
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+}
