e8589359bb59f3e5a549583d13d628cfb02512bf
hendisantika <hendisantika@yahoo.co.id>
2024-04-02 06:59:37 +0700

    Add UserDetailsServiceImpl 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootsecurityjwtauthmongodb/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package id.my.hendisantika.springbootsecurityjwtauthmongodb.service;
+
+import id.my.hendisantika.springbootsecurityjwtauthmongodb.model.User;
+import id.my.hendisantika.springbootsecurityjwtauthmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-auth-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/2/24
+ * Time: 06:59
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
+}
