ee0f0a32a3b9548308db32a948543e1b43d60b87
hendisantika <hendisantika@yahoo.co.id>
2024-04-02 07:01:52 +0700

    Add AuthEntryPointJwt 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootsecurityjwtauthmongodb/security/AuthEntryPointJwt.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.springbootsecurityjwtauthmongodb.security;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-auth-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/2/24
+ * Time: 07:01
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@Slf4j
+public class AuthEntryPointJwt implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws ServletException {
+        log.error("Unauthorized error: {}", authException.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
+    }
+}
