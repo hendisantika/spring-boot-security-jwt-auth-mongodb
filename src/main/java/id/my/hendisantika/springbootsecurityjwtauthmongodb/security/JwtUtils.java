b8b07d6606531bb456acdc09f48edf105faaa127
hendisantika <hendisantika@yahoo.co.id>
2024-04-02 07:04:48 +0700

    Add getUserNameFromJwtToken in JwtUtils 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootsecurityjwtauthmongodb/security/JwtUtils.java
+++ src/main/java/id/my/hendisantika/springbootsecurityjwtauthmongodb/security/JwtUtils.java
@@ -48,4 +48,9 @@ public class JwtUtils {
     private Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key()).build()
+                .parseClaimsJws(token).getBody().getSubject();
+    }
 }
