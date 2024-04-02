b1770b7133ab633094ad1a38a1cfe9cee75fdc51
hendisantika <hendisantika@yahoo.co.id>
2024-04-02 07:10:16 +0700

    Add MessageResponse 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootsecurityjwtauthmongodb/payload/response/MessageResponse.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.springbootsecurityjwtauthmongodb.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-auth-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/2/24
+ * Time: 07:09
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
