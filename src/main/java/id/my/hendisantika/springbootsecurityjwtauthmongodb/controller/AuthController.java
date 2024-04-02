a0571d21f023677a05f9eb64260498d36879787c
hendisantika <hendisantika@yahoo.co.id>
2024-04-02 07:12:58 +0700

    Add registerUser in AuthController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootsecurityjwtauthmongodb/controller/AuthController.java
+++ src/main/java/id/my/hendisantika/springbootsecurityjwtauthmongodb/controller/AuthController.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.springbootsecurityjwtauthmongodb.controller;
 
+import id.my.hendisantika.springbootsecurityjwtauthmongodb.model.ERole;
+import id.my.hendisantika.springbootsecurityjwtauthmongodb.model.Role;
+import id.my.hendisantika.springbootsecurityjwtauthmongodb.model.User;
 import id.my.hendisantika.springbootsecurityjwtauthmongodb.payload.request.LoginRequest;
+import id.my.hendisantika.springbootsecurityjwtauthmongodb.payload.request.SignupRequest;
 import id.my.hendisantika.springbootsecurityjwtauthmongodb.payload.response.JwtResponse;
+import id.my.hendisantika.springbootsecurityjwtauthmongodb.payload.response.MessageResponse;
 import id.my.hendisantika.springbootsecurityjwtauthmongodb.repository.RoleRepository;
 import id.my.hendisantika.springbootsecurityjwtauthmongodb.repository.UserRepository;
 import id.my.hendisantika.springbootsecurityjwtauthmongodb.security.JwtUtils;
@@ -20,7 +25,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -66,6 +73,61 @@ public class AuthController {
                 userDetails.getUsername(),
                 userDetails.getEmail(),
                 roles));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already taken!"));
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Email is already in use!"));
+        }
+
+        // Create new user's account
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<String> strRoles = signUpRequest.getRoles();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "mod":
+                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
     }
 }
 
