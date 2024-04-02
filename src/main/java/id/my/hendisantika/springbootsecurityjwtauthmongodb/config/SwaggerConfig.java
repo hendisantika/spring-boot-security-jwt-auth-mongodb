f4507833e094c00e257a32508074eff7326dda44
hendisantika <hendisantika@yahoo.co.id>
2024-04-02 07:43:38 +0700

    Set SwaggerConfig 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootsecurityjwtauthmongodb/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package id.my.hendisantika.springbootsecurityjwtauthmongodb.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-security-jwt-auth-mongodb
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/2/24
+ * Time: 07:42
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI().info(new Info().title("Demo project for Spring Boot Auth with Spring Security, JWT and MongoDB")
+                .description("Demo project for Spring Boot Auth with Spring Security, JWT and MongoDB")
+                .version("v0.0.2")
+                .contact(getContactDetails()));
+    }
+
+    private Contact getContactDetails() {
+        return new Contact().name("Hendi Santika")
+                .email("hendisantika@yahoo.co.id")
+                .url("https://s.id/hendisantika");
+    }
+}
